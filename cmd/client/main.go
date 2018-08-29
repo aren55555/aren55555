e79dd7d9bcd147c1cebe770ac88871587a02f8fe
Aren Patel <git@arenpatel.com>
2018-08-29 13:51:16 -0600

    Progress.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/aren55555/aren55555/protos"
+	"google.golang.org/grpc"
+)
+
+const (
+	address = "localhost:50051"
+)
 
 func main() {
 	fmt.Println("aren55555 client")
+
+	m := &pb.Message{Body: "Hello World"}
+	fmt.Println(m)
+
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewChatClient(conn)
+
+	fmt.Println(c)
+
+	// Contact the server and print out its response.
+	ctx := context.Background()
+	sc, err := c.Stream(ctx)
+	if err != nil {
+		log.Fatalf("could not get stream client: %v", err)
+	}
+	fmt.Println(sc)
+
+	sc.Send(m)
+
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/aren55555/aren55555/protos"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) Stream(pb.Chat_StreamServer) error {
+	fmt.Println("streaming")
+	return nil
+}
 
 func main() {
 	fmt.Println("aren55555 server")
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterChatServer(s, &server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
